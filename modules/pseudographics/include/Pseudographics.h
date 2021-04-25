7a4a0a9e21e1c46cfaa31f70b65fb9e231f0ebcf
Galkin_Vladislav <vgalk052@mail.ru>
2021-04-25 23:57:56 +0300

    third

--- modules/pseudographics/include/Pseudographics.h
+++ modules/pseudographics/include/Pseudographics.h
@@ -1,4 +1,8 @@
 // Copyright 2021 Galkin Vladislav
+
+#ifndef MODULES_PSEUDOGRAPHICS_INCLUDE_PSEUDOGRAPHICS_H_
+#define MODULES_PSEUDOGRAPHICS_INCLUDE_PSEUDOGRAPHICS_H_
+
 #pragma once
 #include <iostream>
 #include <string>
